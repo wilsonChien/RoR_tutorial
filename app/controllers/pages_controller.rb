2a72cdb81884cd7b5feea0b018fa10aa96a45b4a
wilsonChien <ling5130030@gmail.com>
2015-04-19 23:32:54 +0800

    加入註解以及其他撰寫可能性

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
+	# 當收到 GET /home 或 GET / 會執行
 	def home
 		render 'pages/home'
+		# render 'home'
+		# 省略設定
 	end
 
+	# 當收到 GET /about/me 會執行
 	def about
 		render 'pages/about'
 	end
+
+	# 如果不放入任何的def網站仍可運作
 end
