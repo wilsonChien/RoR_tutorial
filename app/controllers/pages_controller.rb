c6ee998a9adeb8cc1839b58f96636a4fe641ca71
wilsonChien <ling5130030@gmail.com>
2015-04-22 09:46:03 +0800

    制定 layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,17 @@ class PagesController < ApplicationController
 	# 當收到 GET /about/me 會執行
 	def about
 		#render 'pages/about'
+
+		# 兩種方式取得 name 變數
+		@name = "#{params[:first_name]} #{params[:last_name]}"  # => "大豆"
+		#params['name'] # => "大豆"
+		render layout: 'admin' #指定版型
 	end
 
 	# 如果不放入任何的def網站仍可運作
+
+	# 訪問 GET /math 時會執行
+	def math
+		@numbers = (1..46).to_a.sample(6)
+	end
 end
